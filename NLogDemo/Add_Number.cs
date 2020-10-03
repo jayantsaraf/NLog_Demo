b0c188128024a882163065e9ed2747f9a1870961
jayantsaraf <sarafjayant24@gmail.com>
2020-10-03 11:37:14 +0530

    [jayant]Add. added NLog file

---
+++ NLogDemo/Add_Number.cs
@@ -0,0 +1,24 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NLogDemo
+{
+    public class Add_Number
+    {
+        NLog nLog = new NLog();
+        public int Sum(int FirstNumber, int SecondNumber)
+        {
+            int result = FirstNumber + SecondNumber;
+            if (FirstNumber==0 || SecondNumber==0)
+            {
+                nLog.LogDebug("Debug successful : Sum()");
+                nLog.LogError("Input numbers can not be zero");
+                nLog.LogWarn("First and Second number shouldnot be equal to 0");
+                nLog.LogInfo("Sum method passed, Result" + result); 
+            }
+            return result;
+        }
+    }
+}
---
+++ NLogDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NLogDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Addition Problem \n Enter 2 numbers");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            Add_Number add = new Add_Number();
+            Console.WriteLine("Result is" + add.Sum(a,b));
+        }
+    }
+}
